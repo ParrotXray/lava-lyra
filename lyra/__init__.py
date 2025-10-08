daf94b08ed0fa4051fc9508c8c6554ef1dd7ab1d
ParrotXray <b05817691@gmail.com>
2025-10-08 13:06:47 +0800

    fix: Bump version to 1.0.2

--- lyra/__init__.py
+++ lyra/__init__.py
@@ -48,8 +48,8 @@ class _VersionInfo(NamedTuple):
     serial: int
 
 
-__version__ = "1.0.1"
-__version_info__ = _VersionInfo(1, 0, 1, "final", 0)
+__version__ = "1.0.2"
+__version_info__ = _VersionInfo(1, 0, 2, "final", 0)
 
 version = __version__
 version_info = __version_info__
