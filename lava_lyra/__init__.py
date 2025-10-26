6df470dab52065d77ec11c71d3e99db62eeebf5a
ParrotXray <b05817691@gmail.com>
2025-10-27 06:58:40 +0800

    feat: Bump version to 1.4.3

--- lava_lyra/__init__.py
+++ lava_lyra/__init__.py
@@ -48,8 +48,8 @@ class _VersionInfo(NamedTuple):
     serial: int
 
 
-__version__ = "1.4.2"
-__version_info__ = _VersionInfo(1, 4, 2, "final", 0)
+__version__ = "1.4.3"
+__version_info__ = _VersionInfo(1, 4, 3, "final", 0)
 
 version = __version__
 version_info = __version_info__
