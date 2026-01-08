93c6ae1ce71f07c5ce4f299f8a494a3ed5139015
command_cat <131455122+littlecommandcat@users.noreply.github.com>
2026-01-08 10:17:23 +0800

    Add support for Disnake library in checker.py

--- lava_lyra/checker.py
+++ lava_lyra/checker.py
@@ -14,11 +14,16 @@ if TYPE_CHECKING:
     from discord import ApplicationContext as PycordContext
     from discord import Member as PycordMember, User as PycordUser, Guild as PycordGuild
 
-    BotType = "DiscordPyBot" | "PycordBot"
-    ContextType = "DiscordPyContext" | "PycordContext"
-    MemberType = "DiscordPyMember" | "PycordMember"
-    UserType = "DiscordPyUser" | "PycordUser"
-    GuildType = "DiscordPyGuild" | "PycordGuild"
+    # disnake
+    from disnake.ext.commands import Bot as DisnakeBot
+    from disnake.ext.commands import Context as DisnakeContext
+    from disnake import Member as DisnakeMember, User as DisnakeUser, Guild as DisnakeGuild
+
+    BotType = "DiscordPyBot" | "PycordBot" | "DisnakeBot"
+    ContextType = "DiscordPyContext" | "PycordContext" | "DisnakeContext"
+    MemberType = "DiscordPyMember" | "PycordMember" | "DisnakeMember"
+    UserType = "DiscordPyUser" | "PycordUser" | "DisnakeUser
+    GuildType = "DiscordPyGuild" | "PycordGuild" | "DisnakeGuild"
     ClientUserType = BotType
 
 
@@ -42,6 +47,13 @@ class PackageRequirement:
         except PackageNotFoundError:
             return False
 
+    @staticmethod
+    def is_disnake() -> bool:
+        try:
+            version("disnake")
+            return True
+        except PackageNotFoundError:
+            return False
 
 
 def import_discord_types():
@@ -51,11 +63,16 @@ def import_discord_types():
         from discord import VoiceChannel, VoiceProtocol, ClientUser
         return Bot, Context, Member, User, Guild, VoiceChannel, VoiceProtocol, ClientUser
 
-    if PackageRequirement.is_pycord():
+    elif PackageRequirement.is_pycord():
         from discord import Bot, ApplicationContext, Member, User, Guild, VoiceChannel, VoiceProtocol, ClientUser
         return Bot, ApplicationContext, Member, User, Guild, VoiceChannel, VoiceProtocol, ClientUser
 
-    raise RequirementNotFound("Neither discord.py nor py-cord could be found")
+    elif PackageRequirement.is_disnake():
+        from disnake.ext.commands import Bot, Context
+        from disnake import Member, User, Guild, VoiceChannel, VoiceProtocol, ClientUser
+        return Bot, Context, Member, User, Guild, VoiceChannel, VoiceProtocol, ClientUser
+
+    raise RequirementNotFound("Neither discord.py, py-cord nor disnake could be found")
 
 
 Bot, ApplicationContext, Member, User, Guild, VoiceChannel, VoiceProtocol, ClientUser = import_discord_types()
