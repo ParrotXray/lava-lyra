2a0eb21a2656efe1b274699a87ef8d9e39fb7aa4
ParrotXray <b05817691@gmail.com>
2026-02-22 10:37:11 +0800

    chore: update version to 1.6.2 and corresponding version info

--- lava_lyra/__init__.py
+++ lava_lyra/__init__.py
@@ -50,8 +50,8 @@ class _VersionInfo(NamedTuple):
     serial: int
 
 
-__version__ = "1.6.2"
-__version_info__ = _VersionInfo(1, 6, 2, "final", 0)
+__version__ = "1.6.3"
+__version_info__ = _VersionInfo(1, 6, 3, "final", 0)
 
 version = __version__
 version_info = __version_info__
