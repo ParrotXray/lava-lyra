1bec11dec6485c5825772b9c721c248dc7a47ee4
John Kennedy Haringa Peña <jkhp.jkpa@gmail.com>
2026-07-28 08:53:33 +0800

    bugfix: stop + lyrics (nodelink) (#31)

--- lava_lyra/pool.py
+++ lava_lyra/pool.py
@@ -581,7 +581,7 @@ class Node:
         if data and self._is_nodelink and isinstance(data, dict):
             if "encodedTrack" in data and "track" not in data:
                 data["track"] = (
-                    {"encoded": data["encodedTrack"]} if data["encodedTrack"] is not None else None
+                    {"encoded": data["encodedTrack"] if data["encodedTrack"] is not None else None}
                 )
                 del data["encodedTrack"]
             if data.get("endTime") is None:
