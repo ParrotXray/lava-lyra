acce0d3881d27ccc03c6c72f51bc02761a2326ac
ParrotXray <b05817691@gmail.com>
2026-07-30 11:14:49 +0800

    fix: update version to 2.1.1

--- lava_lyra/__init__.py
+++ lava_lyra/__init__.py
@@ -50,8 +50,8 @@ class _VersionInfo(NamedTuple):
     serial: int
 
 
-__version__ = "2.1.0"
-__version_info__ = _VersionInfo(2, 1, 0, "final", 0)
+__version__ = "2.1.1"
+__version_info__ = _VersionInfo(2, 1, 1, "final", 0)
 
 version = __version__
 version_info = __version_info__
